153d0b8eae1507670279c7049e3ab52180b8c579
Sanaginoya <yangxuance@gmail.com>
2023-10-10 20:27:50 +0800

    text commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d", &a);
+    b = 3;
+    c = a + b;
+    printf("a = %d, b = %d, c=%d.\n", a, b, c);
+    printf("hello, world.\n");
+}
